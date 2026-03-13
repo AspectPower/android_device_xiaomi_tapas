7f1d2c9226285a4c2a77e5f2a1b157880f21b304
Deivid21 <david.parra.ignacio@gmail.com>
2026-03-13 09:26:52 +0000

    tapas: Patch vidhance components with libui-v34 * EIS was broken, which caused hard crashes when switching   to video mode in camera. Signed-off-by: Masood-J <m.masood7679@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -61,9 +61,14 @@ blob_fixups: blob_fixups_user_type = {
 
     'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup()
         .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-fromvendor.so'),
-
     'system_ext/lib64/libprotobuf-cpp-full-fromvendor.so': blob_fixup()
         .replace_needed('libprotobuf-cpp-lite.so','libprotobuf-cpp-lite-fromvendor.so'),
+    (
+        'vendor/lib64/camera/components/com.qti.node.dewarp.so',
+        'vendor/lib64/camera/components/com.vidhance.node.processing.so',
+    ): blob_fixup()
+        .replace_needed('libui.so', 'libui-v34.so'),
+
 }
 
 module = ExtractUtilsModule(
